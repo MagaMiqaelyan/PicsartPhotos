c3322a66ef95aaf74d375ebfe75d8a7fd6487e63
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-09-06 02:38:42 +0400

    photo

---
+++ PicsartPhotos/PicsartPhotos/Photo.cs
@@ -0,0 +1,25 @@
+﻿namespace PicsartPhotos
+{
+    public class Photo
+    {
+        public string ID { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string[] Tags { get; set; }
+        public bool Mature { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Date { get; set; }
+        public User User { get; set; }
+        public string Type { get; set; }
+        public bool Public { get; set; }
+        public int LikesCount { get; set; }
+        public int ViewsCount { get; set; }
+        public int CommentsCount { get; set; }
+        public int StreamsCount { get; set; }
+        public int ForksCount { get; set; }
+        public int SourcesCount { get; set; }
+        public bool ShowEditHistory { get; set; }
+        public bool HasSimilars { get; set; }
+    }
+}
---
+++ PicsartPhotos/PicsartPhotos/User.cs
@@ -0,0 +1,11 @@
+﻿namespace PicsartPhotos
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public bool Is_verified { get; set; }
+    }
+}
